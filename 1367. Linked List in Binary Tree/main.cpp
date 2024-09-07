fc87271462e8812832c77ad70a3ff6fc40075ba7
bpn2k4 <bpn2k4@gmail.com>
2024-09-07 13:54:13 +0700

    1367. Linked List in Binary Tree

---
+++ 1367. Linked List in Binary Tree/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  void solve(vector<int> &nums)
+  {
+    return;
+  }
+};
+
+int main()
+{
+  Solution s;
+  vector<int> nums = {1, 2, 3, 4, 5};
+  s.solve(nums);
+  for (int num : nums)
+  {
+    cout << num << " ";
+  }
+  return 0;
+}
